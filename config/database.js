c56f464d4193f840a2890cb6caccc5b6c1766716
JCasaisCar <jesuscasacarrillo@gmail.com>
2025-03-01 18:32:01 +0100

    solucionamos la advertencia de variables obsoletas

--- config/database.js
+++ config/database.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Establezco la conexión con MongoDB usando las variables de entorno
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB conectado correctamente")) // Mensaje si la conexión es exitosa
     .catch(err => console.error("❌ Error en la conexión a MongoDB:", err)); // Mensaje si hay error
 
